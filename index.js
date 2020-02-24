4fc5e9941c4a05c1f4e64120e76b80df2e00564f
joseplopezderecaldemartorell <josep.lopez.de.recalde.martorell@everis.com>
2020-02-24 17:16:07 +0100

    test resize computation of vh

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 "use strict";
 
-var title = document.getElementById('title');
-var nextPage = document.getElementById('next-page');
+var title = document.getElementById("title");
+var nextPageArrow = document.getElementById("next-page");
 
-title.addEventListener('click',()=>{
-    document.location.href = '/';
-})
+title.addEventListener("click", () => {
+  document.location.href = "/";
+});
 
+nextPageArrow.addEventListener("click", changePageWithParams);
 
-nextPage.addEventListener('click',(ev)=> {
-    console.log(document.location);
-    const path = document.location.pathname;
-    const param = path.substring(1);
-    if(param !== ''){
-        const nextPage = parseInt(param) + 1;
-        document.location.href = origin + '/' + nextPage.toString();
-    }else{
-        const origin = document.location.origin;
-        document.location.href = origin + '/1';
-    }
-})
+let vh = window.innerHeight * 0.01;
+
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+window.addEventListener("resize", () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
+
+function changePageWithParams() {
+  const path = document.location.pathname;
+  const param = path.substring(1);
+  if (param !== "") {
+    const nextPage = parseInt(param) + 1;
+    document.location.href = origin + "/" + nextPage.toString();
+  } else {
+    const origin = document.location.origin;
+    document.location.href = origin + "/1";
+  }
+}
